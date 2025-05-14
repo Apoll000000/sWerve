815fb199f66241fa5f8e269c3a3afe394af290af
Apollo <leonenpatrickpaul@gmail.com>
2025-05-15 01:25:17 +0800

    EWAN KO NA POTA

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,23 @@
 // src/utils/auth.ts
 import { supabase } from '@/lib/supabase';
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    '${window.location.origin}/auth/callback'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
 export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`, // optional: handle redirect
+      redirectTo: getURL(), // optional: handle redirect
     },
   });
 
@@ -20,7 +32,7 @@ export const signInWithFacebook = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'facebook',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`,
+      redirectTo: getURL(),
     },
   });
 
