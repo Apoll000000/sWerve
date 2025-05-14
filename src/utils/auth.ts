c75117f99fa5b8d8e2953825452cf999405616ca
Apollo <leonenpatrickpaul@gmail.com>
2025-05-15 01:31:23 +0800

    EWAN KO NA TALGA POTA

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,8 +3,8 @@ import { supabase } from '@/lib/supabase';
 
 const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VITE_SUPABASE_URL! ?? 'https://swerve-teal.vercel.app/' // Set this to your site URL in production env.
+    process?.env?.VITE_SUPABASE_URL! ?? // Automatically set by Vercel.
     '${window.location.origin}/auth/callback'
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
